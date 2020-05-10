bd8ae64086426385b372475f753c910e45ddd0a8
pinpinpin <benjamard.mee@gmail.com>
2020-05-10 20:37:36 +0700

    generate new component for country-list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CountryListComponent } from './pages/country-list/country-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CountryListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
