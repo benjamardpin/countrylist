771a88e9e50de4956a70291cfe757f35f22868da
pinpinpin <benjamard.mee@gmail.com>
2020-05-11 00:32:49 +0700

    [Create] method for search

--- src/app/pages/country-list/country-list.component.ts
+++ src/app/pages/country-list/country-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Country } from 'src/app/models/country';
+import { CountryService } from 'src/app/services/country.service';
 @Component({
   selector: 'app-country-list',
   templateUrl: './country-list.component.html',
   styleUrls: ['./country-list.component.scss']
 })
 export class CountryListComponent implements OnInit {
-
-  constructor() { }
+  searchModel: string;
+  countries: Country[];
+  constructor(private countryService: CountryService) {}
 
   ngOnInit(): void {
+    this.countryService.getCountries().subscribe((countries) => {
+      this.countries = countries;
+      console.log(countries)
+    })
+  }
+  updateSearchModel(value) {
+    this.searchModel = value;
   }
 
 }
