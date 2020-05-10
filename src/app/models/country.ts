71eeee6738cee4b2ac03612f94592e7385e81a6d
pinpinpin <benjamard.mee@gmail.com>
2020-05-11 00:32:36 +0700

    [Create] CountryService

---
+++ src/app/models/country.ts
@@ -0,0 +1,9 @@
+export interface Country {
+    name: string;
+    region: string;
+    subregion: string;
+    population: number;
+    latlng: number[];
+    area: number;
+    nativeName: string;
+}
--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/environment'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Country } from '../models/country';
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
+  url = environment.apiUrl
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(`${this.url}/all`)
+  }
 }
